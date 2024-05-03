f7fa511b07b2d32ffced58687b564b1ddf717187
Ramon.god <ramongodinho01@gmail.com>
2024-05-03 19:34:09 -0300

    contantes e variaveis teste

---
+++ variaveis_constantes.py
@@ -0,0 +1,11 @@
+nome = 'Ramon'
+idade = 21
+
+nome = 'Lucas'
+print(nome, idade)
+
+limite_de_cadastro = 10
+
+BRASIL_STATES = ['SP', 'RJ', 'MG', 'ES', 'RS', 'SC', 'PR', 'MT', 'MS', 'GO', 'DF', 'TO', 'RO', 'AC', 'AM', 'RR', 'PA', 'AP', 'MA', 'PI', 'CE', 'RN', 'PB', 'PE', 'AL', 'SE', 'BA']
+
+print(BRASIL_STATES)
