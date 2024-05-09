1b0f6ccba85fc44d7b347f69d1fbd312734bd0ba
Ramon.god <ramongodinho01@gmail.com>
2024-05-08 22:02:29 -0300

    brincando com string

---
+++ strings.py
@@ -0,0 +1,15 @@
+nome = '         rolinux'
+
+print(nome.upper())
+print(nome.lower())
+print(nome.capitalize())
+print(nome.title())
+print(nome.swapcase())
+print(nome.strip())
+print(nome.lstrip())
+print(nome.rstrip())
+print(nome.split())
+
+texto = "  Python  "
+texto_sem_espaco = texto.lstrip()
+print(texto_sem_espaco)
